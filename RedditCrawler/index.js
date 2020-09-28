e15e83b443a27ea60e932f91489edef6564c39f6
Martin <marb61a@gmail.com>
2020-09-28 12:03:31 +0100

    added nested comments expanding to index.js

--- RedditCrawler/index.js
+++ RedditCrawler/index.js
@@ -18,10 +18,30 @@ const url = 'https://old.reddit.com/r/learnprogramming/comments/4q6tae/i_highly_
     // Expand all comment threads on the page
     // $ will do the same as document.querySelector
     // $$ will do the same as document.querySelectorAll
-    const expandButtons = await page.$$('.morecomments');
-    console.log(expandButtons.length);
+    let expandButtons = await page.$$('.morecomments');
+    // console.log(expandButtons.length);
 
-    for(let button of expandButtons){
+    // Needed to keep opening more comments
+    while(expandButtons.length){
+        for(let button of expandButtons){
+            // Click buttons to expand all nested comments
+            // This will be set to loading as Reddit is rate throttled
+            // As there are 59 comments this will cause a problem on Reddit server
+            await button.click();
+
+            // Adding a small wait between clicks should help with loading problems
+            await page.waitFor(500);
+        }
+
+        // Need to add wait to avoid document detachment
+        await page.waitFor(1000);
+
+        expandButtons = await page.$$('.morecomments');
+    }
+
+    // Select all comments and scrape text + points
+    const comments = await page.$$('.entry');
+    for(let comment of comments){
         
     }
 
